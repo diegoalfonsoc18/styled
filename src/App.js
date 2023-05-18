75a0eaea04ad05b93e22ab33822cd4978f1ee7db
Diego Alfonso <dlalfonso57@hotmail.com>
2023-05-18 17:49:47 -0500

    theme colors

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
-
+import { temaClaro, temaOScuro } from "./Components/UI/temas";
 import Header from "./Components/Header";
 import Container from "./Components/Container";
+import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./GlobalStyle";
+
+console.log(temaClaro, temaOScuro);
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={temaOScuro}>
       <GlobalStyle />
       <Header />
       <Container />
-    </>
+    </ThemeProvider>
   );
 }
 
